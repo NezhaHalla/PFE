b67372e589648d99b3cd711b85c38c23b14737e2
soukainalam <lamsiahsoukaina7@gmail.com>
2024-03-18 23:54:13 +0000

    a

--- ScholarNet/resources/views/teacher/assignment_details.blade.php
+++ ScholarNet/resources/views/teacher/assignment_details.blade.php
@@ -1,64 +1,8 @@
 <x-master :title="$assignment->titre">
     <head>
         <link href="{{ asset('css/details.css') }}" rel="stylesheet">
-        <style>
-            /* Add your custom styles here */
-            .card-list {
-                display: flex;
-                justify-content: center;
-                align-items: center;
 
-                height: calc(80vh - 100px);
-            }
 
-            .card {
-                background-color: #f9f9f9;
-                border-radius: 10px;
-                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                padding: 20px;
-                max-width: 400px;
-                width: 100%;
-                margin: 0 auto;
-            }
-
-            .card-header {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                margin-bottom: 10px;
-            }
-
-            .card-image {
-                text-align: center;
-                margin-bottom: 20px;
-            }
-
-            .card-image img {
-                max-width: 100%;
-                border-radius: 8px;
-            }
-
-            .card-image div {
-                margin-bottom: 10px;
-            }
-
-            .doc {
-                margin-top: 20px;
-            }
-
-            .sup {
-                background-color: #f44336;
-                color: white;
-                border: none;
-                border-radius: 5px;
-                padding: 10px 20px;
-                cursor: pointer;
-            }
-
-            .sup:hover {
-                background-color: #d32f2f;
-            }
-        </style>
     </head>
     <body>
         <div class="card-list">
